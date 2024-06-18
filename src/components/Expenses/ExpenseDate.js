0d46ed89f48f31037abd2d1a215c48a88523f0a2
Lugeja <aaronalev@gmail.com>
2024-06-19 00:17:32 +0300

    9. Komponentide failide organiseerimine projektis ja muudatused funktsioonide kirjutusviisil

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,6 @@
 import './ExpenseDate.css';
 
-function expenseDate(props) {
+const expenseDate = (props) => {
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const year = props.date.getFullYear();
