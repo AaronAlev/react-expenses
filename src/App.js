68e38530f343bc09ec491220643b869689bbe007
Lugeja <aaronalev@gmail.com>
2024-06-18 23:45:00 +0300

    3. Komponendi sisustamine andmetega

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [{
+    date: new Date(2023, 9, 6),
+    title: 'Car Insurance',
+    amount: 294.67
+  },
+  {
+    date: new Date(2023, 9, 7),
+    title: 'Library book overdue',
+    amount: 1.45
+  }];
   return (
     <div className='App'>
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem 
+        expenseData={expenses[0]}
+      />
+      <ExpenseItem 
+        expenseData={expenses[1]}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  return (
-    <div className='expense-item'>
-        <div>Date</div>
-        <div className='expense-item__description'>
-            <h2>Title</h2>
-            <div className='expense-item__price'>Amount</div>
+function ExpenseItem(props) {
+    return (
+        <div className='expense-item'>
+            <div>{props.expenseData.date.toString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.expenseData.title}</h2>
+                <div className='expense-item__price'>{props.expenseData.amount}</div>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default ExpenseItem;
