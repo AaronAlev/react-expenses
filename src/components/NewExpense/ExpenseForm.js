f6355e7aa8b18f022e8ee01d9cd3f45b465ccc29
Lugeja <aaronalev@gmail.com>
2024-06-19 12:02:15 +0300

    17. Fragment, Portals  ja useRef hooki kasutamine

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,14 +1,20 @@
 import './ExpenseForm.css';
-import { useState } from 'react';
+import { Fragment, useState, useRef } from 'react';
+import Error from '../UI/Error';
 
 const ExpenseForm = (props) => {
     const [isEditing, setIsEditing] = useState(false);
+    const [error, setError] = useState(null);
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    //const [enteredTitle, setEnteredTitle] = useState('');
+    //const [enteredAmount, setEnteredAmount] = useState('');
+    //const [enteredDate, setEnteredDate] = useState('');
 
-    const titleChangeHandler = (event) => {
+    const titleInputRef = useRef();
+    const amountInputRef = useRef();
+    const dateInputRef = useRef();
+
+    /*const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
     };
     const amountChangeHandler = (event) => {
@@ -16,43 +22,74 @@ const ExpenseForm = (props) => {
     };
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+    };*/
+
+    const errorHandler = () => {
+        setError(null);
     };
 
     const submitHandler = (event) => {
+        const enteredTitle = titleInputRef.current.value;
+        const enteredAmount = amountInputRef.current.value;
+        const enteredDate = dateInputRef.current.value;
         event.preventDefault();
+
+        if(enteredTitle.trim().length === 0 || enteredAmount.trim().length === 0 || enteredDate.trim().length === 0){
+            setError({
+                title: 'Invalid Input',
+                message: 'Please enter a valid title, amount and date (non-empty values).'
+            });
+            console.log(error);
+            return;
+        }
+
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
         props.onSaveExpenseData(expenseData);
-        setEnteredTitle('');
-        setEnteredAmount('');
-        setEnteredDate('');
+        titleInputRef.current.value= '' ;
+        amountInputRef.current.value= '' ;
+        dateInputRef.current.value= '' ;
     };
 
     return (
         <>
+        <Fragment>
+            { error && (
+                <Error
+                    title={error.title}
+                    message={error.message}
+                    onConfirm={errorHandler}
+                />
+            )}
         {isEditing && (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
                     <input type='text' 
-                    onChange={titleChangeHandler}
-                    value={enteredTitle}/>
+                    //onChange={titleChangeHandler}
+                    //value={enteredTitle}
+                    id='title'
+                    ref={titleInputRef}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
                     <input type='number' min='0.01' step='0.01' 
-                    onChange={amountChangeHandler}
-                    value={enteredAmount}/>
+                    //onChange={amountChangeHandler}
+                    //value={enteredAmount}
+                    id='amount'
+                    ref={amountInputRef}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
                     <input type='date' min='2023-01-01' max='2025-01-31' 
-                    onChange={dateChangeHandler}
-                    value={enteredDate}/>
+                    //onChange={dateChangeHandler}
+                    //value={enteredDate}
+                    id='date'
+                    ref={dateInputRef}/>
                 </div>
             </div>
             <div className='.new-expense__actions'>
@@ -64,7 +101,7 @@ const ExpenseForm = (props) => {
         {!isEditing && (
             <button onClick={() => setIsEditing(true)}>Add New Expense</button>
         )}
-
+        </Fragment>
         </>
     )
 }
